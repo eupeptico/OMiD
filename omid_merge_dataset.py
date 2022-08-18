831fc7b2dd534c71c322ba5de6250974fb0adf28
eupeptico <eugeniocorallo@me.com>
2022-08-18 14:48:10 +0200

    commit

--- omid_merge_dataset.py
+++ omid_merge_dataset.py
@@ -4,7 +4,8 @@ import re
 
 #the below identifies the text as main, for when it will be imported by other .py
 if __name__ == "__main__":
-
+    
+    #make sure to identify the right datasets in .csv format
     concerts = pd.read_csv("./datasets/concerts.csv")
     works = pd.read_csv("./datasets/works.csv")
     solo = pd.read_csv("./datasets/soloists.csv")
@@ -20,17 +21,17 @@ if __name__ == "__main__":
     works["ComposerName"] = works["ComposerName"].apply(lambda x : re.sub("[\(\[].*?[\)\]]", "", x))
     solo["Soloist_Name"] = solo["Soloist_Name"].apply(lambda x : re.sub("[\(\[].*?[\)\]]", "", x))
 
-    
+    #delete lines with empty values (in soloists.csv)
+
     #merging dataframes
     dfs=[concerts,works,solo]
     merged = reduce(lambda left, right: pd.merge(left, right, on='ID'), dfs)
     #reduce(func,seq) applies a function "func" on all elements of a sequence, in this case a list of pd.DataFrame
-    #"func" is a lambda function having 2 inputs and returning pd.merge(left,right,on="ID") a pandas function
+    #"func" is a lambda function having 2 inputs and returning pd.merge (left,right,on="ID") a pandas function
     #that unites two dataframes having a common header ( in this case "ID")
 
     #dropping duplicates with a pandas function designed to do so.
     merged.drop_duplicates()
-
 
     with open("./datasets/merged.csv","w") as file_out:
         merged.to_csv(file_out, encoding="utf-8")
