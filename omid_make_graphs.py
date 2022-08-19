788f5ceb63434c04abc57a82353e642cec5531e8
edocorallo <edoardo.corallo@gmail.com>
2022-08-19 12:54:59 +0200

    added comment for later

--- omid_make_graphs.py
+++ omid_make_graphs.py
@@ -236,3 +236,35 @@ if __name__ == "__main__":
     # show_top_by_total_duration(orchestra_dataframe,"Soloist_Name")
     # daily_durations(orchestra_dataframe)
     events_per_month(orchestra_dataframe)
+
+
+
+    #PER DOPO
+    #from matplotlib import pyplot as plt
+    # import numpy as np
+    
+    # # Creating dataset
+    # marks = np.array([70, 50, 40, 90, 55, 85, 74, 66, 33, 11, 45, 36, 89])
+    
+    # # Creating histogram
+    # fig, ax = plt.subplots(1, 1)
+    # ax.hist(marks)
+    
+    # # Set title
+    # ax.set_title("Title")
+    
+    # # adding labels
+    # ax.set_xlabel('x-label')
+    # ax.set_ylabel('y-label')
+    
+    # # Make some labels.
+    # rects = ax.patches
+    # labels = ["label%d" % i for i in range(len(rects))]
+    
+    # for rect, label in zip(rects, labels):
+    #     height = rect.get_height()
+    #     ax.text(rect.get_x() + rect.get_width() / 2, height+0.01, label,
+    #             ha='center', va='bottom')
+    
+    # # Show plot
+    # plt.show()
